fe5b6e587020356473fec51aa41172898bbd689f
Kelly Li <100189509+kellylikeyu@users.noreply.github.com>
2022-09-02 12:36:57 -0700

    Update 20220610 Merge overlapping intervals.js

--- 20220610 Merge overlapping intervals.js
+++ 20220610 Merge overlapping intervals.js
@@ -2,6 +2,32 @@
 //each interval is an array of two integers, with interval[0] as the start of the interval and interval[1] as the end of interval.
 //[1,5] and [6,7] are not overlapping. [1,6] and [6,7] are overlapping.
 //sample input: intervals = [[1,2],[3,5],[4,7],[6,8],[9,10]]   sample output: [[1,2],[3,8],[9,10]]
+
+function mergeOverlappingIntervals(array) {
+  // Write your code here.
+	const sorted = array.sort((a,b)=> a[0]-b[0]);
+    let merged = [array[0]];
+  let mergedEnding = [array[0][1]];
+  for (let i=1; i<array.length; i++) {
+    let latestEnd = Math.max(...mergedEnding);
+    if (array[i][0] > latestEnd) {
+      merged.push(array[i]);
+      mergedEnding.push(array[i][1]);
+    }
+    else {
+      let index = mergedEnding.indexOf(latestEnd);
+      let curEnd = Math.max(latestEnd, array[i][1]);
+      merged[index] = [merged[index][0],curEnd];
+      mergedEnding[index] = curEnd;
+    }
+  }
+  return merged;
+}
+
+
+
+
+
 function mergeOverlappingIntervals(array) {
 	if (array.length === 1) {
 		return array;
